b0d8402fe338200e92aba2cf4e7a03a05ded6944
Veda Ramaiah <vedarsh.gangatkar@gmail.com>
2018-03-16 16:57:32 -0400

    Bootstrap Social Media Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ExploreComponent } from './explore/explore.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ExploreComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path:"home",component:HomeComponent},
+      {path:"explore",component:ExploreComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
